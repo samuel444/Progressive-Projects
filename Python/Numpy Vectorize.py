8294554a243ea32089f4277d41da64546c04480a
Samuel Whitby <sw2890@bath.ac.uk>
2026-06-19 15:28:54 +0100

    Minor Presentation Change

--- Python/Numpy Vectorize.py
+++ Python/Numpy Vectorize.py
@@ -1,14 +1,17 @@
 import numpy as np
 
+a = np.array([1, 2, 3, 4, 5])
+
+# Vectorization
 def power(x):
     return x ** 4
 
 v_power = np.vectorize(power)
 
-a = np.array([1, 2, 3, 4, 5])
 result = v_power(a)
 print(result) 
 
+# Vectorization with excluded arguments
 def power_exclude(x, powers):
     return x ** powers
 
@@ -16,6 +19,7 @@ v_power_exclude = np.vectorize(power_exclude, excluded=['powers'])
 result = v_power_exclude(a, 5)
 print(result)
 
+# Vectorization with output types
 def power_otype(x):
     return (x % 2) == 0
 
