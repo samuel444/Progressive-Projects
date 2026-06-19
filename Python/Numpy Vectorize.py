87973b2ea8ea1d8ac24e5021cd67e539277800de
Samuel Whitby <sw2890@bath.ac.uk>
2026-06-19 15:26:55 +0100

    Adding Otype and Exclude

--- Python/Numpy Vectorize.py
+++ Python/Numpy Vectorize.py
@@ -12,6 +12,14 @@ print(result)
 def power_exclude(x, powers):
     return x ** powers
 
-v_power_exclude = np.vectorize(power_exclude)
-result = v_power_exclude(a, 5) 
+v_power_exclude = np.vectorize(power_exclude, excluded=['powers'])
+result = v_power_exclude(a, 5)
 print(result)
+
+def power_otype(x):
+    return (x % 2) == 0
+
+v_power_otype = np.vectorize(power_otype, otypes=[bool])
+result = v_power_otype(a)
+print(result)
+
