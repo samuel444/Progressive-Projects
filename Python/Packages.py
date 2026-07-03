ba254fbc522d30a396f13d7a3fbaab06af90c34a
Samuel Whitby <sw2890@bath.ac.uk>
2026-07-03 14:47:51 +0200

    adding comment about directory in packages

--- Python/Packages.py
+++ Python/Packages.py
@@ -3,6 +3,10 @@ import pandas as pd
 import sys
 
 sys.path.append("/Users/sam/Progressive-Projects/Packages")
+# Above only works on my computer, so if your using it, you may want to change 
+# the path to your own directory where you have the my_packages.py file, or:
+# project_root = Path(__file__).resolve().parents[1]
+# sys.path.append(str(project_root / "Packages"))
 
 import my_packages as mp
 
