17ffa3e84ca1bb18851a61d3701585677e8320b2
Samuel Whitby <sw2890@bath.ac.uk>
2026-07-05 19:32:25 +0100

    Final Draft Cleaning Data

--- Python/Cleaning Data.py
+++ Python/Cleaning Data.py
@@ -6,7 +6,7 @@ df = pd.DataFrame({
     "Name": ["Sam", "Alice", "Bob", "Alice", "Tom"],
     "Age": [25, np.nan, "30", np.nan, 200],
     "Gender": ["Male", " female ", "MALE", " female ", "Male"],
-    "Salary": [30000, 40000, 35000, 40000, 500000]
+    "Salary": [30000, '40000', 35000, 40000, 500000]
 })
 
 print("\nORIGINAL DATA")
@@ -14,10 +14,14 @@ print(df)
 
 # MISSING VALUES
 print("\n--- Missing Values ---")
+print(df.isnull())
 print(df.isnull().sum())
 
 # Convert Age to numeric first
 df["Age"] = pd.to_numeric(df["Age"], errors="coerce")
+
+# Drop the rows with missing values
+print(df.dropna())
 
 # Fill missing values with median age
 median_age = df["Age"].median()
@@ -30,6 +34,7 @@ print(df)
 print("\n--- Duplicate Rows ---")
 print(df.duplicated())
 
+# Drop duplicate rows
 df = df.drop_duplicates()
 
 print("\nAfter Removing Duplicates:")
@@ -39,11 +44,47 @@ print(df)
 print("\n--- Data Types ---")
 print(df.dtypes)
 
-# Ensure Age is numeric
-df["Age"] = pd.to_numeric(df["Age"])
 
+# Ensure Salary is numeric
+df['Salary'] = pd.to_numeric(df['Salary'], errors='coerce')
+
+# Other method: Make salary type int64
+df['Salary'] = df['Salary'].astype('int64')
+
+# Check data types again
 print("\nData Types After Conversion:")
 print(df.dtypes)
+
+# OUTLIERS
+print("\n--- Outliers ---")
+
+for col in df.columns:
+    if df[col].dtype in ['int64', 'float64']:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+
+        outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)]
+
+        print("\nColumn Outliers: "+col)
+        print(outliers)
+
+
+# Remove the row instead
+print("\nAfter Removing Outliers:")
+print(df[df["Age"] <= 120])
+
+# Outlier Removal using drop
+print(df.drop(index=4))
+
+# Correct the value if you KNOW it's wrong
+df.loc[df["Age"] == 200, "Age"] = 20
+
+print("\nAfter Correcting the Age:")
+print(df)
 
 # INCONSISTENT TEXT
 print("\n--- Gender Values Before Cleaning ---")
@@ -58,3 +99,5 @@ df["Gender"] = (
 print("\nGender Values After Cleaning:")
 print(df["Gender"].unique())
 
+print("\nCleaned Dataset:")
+print(df)
