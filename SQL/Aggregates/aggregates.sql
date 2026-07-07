b4823ca463367c2bc5d732d6e5983f8bd6d7d761
Samuel Whitby <sw2890@bath.ac.uk>
2026-07-07 23:35:28 +0100

    second aggregates changes

--- SQL/Aggregates/aggregates.sql
+++ SQL/Aggregates/aggregates.sql
@@ -27,3 +27,28 @@ customer_id,
 SUM(quantity)
 FROM Orders
 GROUP BY customer_id;
+
+
+SELECT
+customer_id,
+MAX(quantity)
+FROM Orders
+GROUP BY customer_id;
+
+
+SELECT
+product_id,
+customer_id,
+SUM(quantity)
+FROM Orders
+GROUP BY
+product_id,
+customer_id;
+
+
+SELECT
+customer_id,
+SUM(quantity) AS total_quantity
+FROM Orders
+GROUP BY customer_id
+ORDER BY total_quantity DESC;
