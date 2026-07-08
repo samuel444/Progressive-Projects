7b0f624b3cc28f8a0c24633ac04afb7e0aaf5234
Samuel Whitby <sw2890@bath.ac.uk>
2026-07-08 17:39:47 +0100

    initial rolling averages

---
+++ SQL/Rolling Averages/Rolling Averages.sql
@@ -0,0 +1,43 @@
+-- Return of the week, ending current day
+SELECT
+    day,
+    share_price_change,
+    SUM(share_price_change) OVER (
+        ORDER BY day
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS last_week_return
+FROM SharePrices;
+
+
+-- Return of the week, starting current day
+SELECT
+    day,
+    share_price_change,
+    SUM(share_price_change) OVER (
+        ORDER BY day
+        ROWS BETWEEN CURRENT ROW AND 6 FOLLOWING
+    ) AS next_week_return
+FROM SharePrices;
+
+
+-- Return of the previous 3 days, current day, and next 3 days
+SELECT
+    day,
+    share_price_change,
+    SUM(share_price_change) OVER (
+        ORDER BY day
+        ROWS BETWEEN 3 PRECEDING AND 3 FOLLOWING
+    ) AS return_previous_3_current_next_3
+FROM SharePrices;
+
+
+-- Return of the running total from the first day to the current day
+SELECT
+    day,
+    share_price_change,
+    SUM(share_price_change) OVER (
+        ORDER BY day
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS running_return
+FROM SharePrices;
+
