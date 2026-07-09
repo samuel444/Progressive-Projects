c57a80cc43f3b33954a1a767de9c6162ae02aa7d
Samuel Whitby <sw2890@bath.ac.uk>
2026-07-09 01:17:00 +0100

    Initial Rolling Features

---
+++ Time Series and Feature Engineering/Rolling Features.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+import yfinance as yf
+
+apple = yf.download("AAPL", period="400d", interval="1d")
+
+apple["Rolling Week Mean"] = apple["Close"].rolling(window=7).mean()
+apple["Rolling Quarter Median"] = apple["Close"].rolling(window=90).median()
+apple["Rolling Month Std"] = apple["Close"].rolling(window=30).std()
+apple["Rolling Year Min"] = apple["Close"].rolling(window=365).min()
+apple["Rolling Max"] = apple["Close"].expanding().max()
+
+apple = apple.dropna()
+
+
+print("\nApple Last 30 Days Data:")
+print(apple[["Close", "Rolling Week Mean", "Rolling Quarter Median", "Rolling Month Std", "Rolling Year Min", "Rolling Max"]].tail(30))
