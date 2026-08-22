dc45119410494bdea1a56be119ba742b48223939
Samuel Whitby <sw2890@bath.ac.uk>
2026-08-22 20:30:46 +0100

    Remove Duplicate Option Pricer

--- Projects/Option Pricer N Risk Engine/src/options_risk_engine/__init__.py
+++
@@ -1,7 +0,0 @@
-
-"""Options pricing and portfolio risk engine."""
-
-from .config import PROJECT_ROOT, ProjectConfig
-from .domain import OptionTicker
-
-__all__ = ["PROJECT_ROOT", "ProjectConfig", "OptionTicker"]
--- Projects/Option Pricer N Risk Engine/src/options_risk_engine/reporting/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from .plots import *
-from .tables import *
